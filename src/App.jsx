05ec0dc0f30c62de51049b71be2ca14f1c6a75f0
Meir Soskil <meirsoskil@gmail.com>
2023-10-10 17:42:26 -0400

    linked to supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 
-import "./App.css";
+const supabase = createClient(
+  "https://caanhmrdxdwqpumrwxjt.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhYW5obXJkeGR3cXB1bXJ3eGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NjU5NzQsImV4cCI6MjAxMjU0MTk3NH0.q1wkSPmAnMr2BcTsEk9fdvN1xslvYTozVP9mDFO81CA"
+);
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const { data } = await supabase.from("Users").select();
+    setUsers(data);
+  }
+
   return (
-    <>
-      <h1>EcoQuest</h1>
-    </>
+    <ul>
+      {users.map((user) => (
+        <li key={user.username}>{user.username}</li>
+      ))}
+    </ul>
   );
 }
 
