32d97a18bbff2823b2d49419587afa44ee0e277b
Aleytys Isbell <isbellaleytys@gmail.com>
2023-10-12 17:01:05 -0400

    font and react helmet in layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,25 @@
-import React from "react";
 
 function About() {
-  return <div>Our mission</div>;
+  return (
+    <>
+  <div className="flex justify-between w-70 p-8 pb-12">
+    <div className="flex flex-col pl-20 pr-20 bg-primary rounded-3xl">
+    <h1 className="text-slate-200 text-6xl mt-8">Our Mission</h1>
+    <p className="text-2xl mt-10 w-80">At EcoQuest, we empower individuals to make sustainable choices for a healthier planet. 
+      Our mission is to provide users with tools, knowledge, and inspiration to reduce their carbon footprint, 
+      conserve resources, and embrace eco-friendly living. Through our environmental calculator, 
+      educational content, challenging quests, and a global community, we facilitate positive 
+      change one user at a time, making sustainability accessible and rewarding for all.</p>
+    </div>
+    <div className="flex ">
+    <div className= "bg-primary rounded-3xl pl-20 pr-20 flex flex-col justify-between">
+second column
+<div className= "bg-primary rounded-3xl h-3/4"> last column</div>
+    </div>
+  </div>
+  </div>
+  </>
+  )
 }
 
 export default About;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import { Helmet } from "react-helmet";
 import NavBar from "./NavBar";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex w-screen">
       <NavBar />
-      <div className="w-full flex justify-center">{children}</div>
+      <div className="w-full flex justify-center">
+        <Helmet>
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossorigin
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Gluten:wght@300&family=Lato&family=Raleway&family=VT323&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        {children}
+      </div>
     </div>
   );
 };
+
 export default Layout;
+
+
