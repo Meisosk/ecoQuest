8ea8c23c5dc1dcd4347da3b288d5c2e01811012e
Meir Soskil <meirsoskil@gmail.com>
2023-10-13 15:18:07 -0400

    fixed some random things and added button color

--- src/components/QuestsPage.jsx
+++ src/components/QuestsPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../App"
+import { supabase } from "../App";
 import GetQuests from "../GetQuests";
 
 function QuestsPage() {
@@ -24,14 +24,16 @@ function QuestsPage() {
   const handleChoice = async (questId) => {
     const user = supabase.auth.user();
     const userId = user ? user.id : null;
-  
+
     if (!userId) {
-      console.error('User is not logged in');
+      console.error("User is not logged in");
       return;
     }
     const updatedQuests = quests.map((quest) => {
       if (quest.id === questId) {
-        supabase.from('Accepted').insert([{ user_id: userId, quest_id: questId }]);
+        supabase
+          .from("Accepted")
+          .insert([{ user_id: userId, quest_id: questId }]);
         return { ...quest, status: "accepted" };
       }
       return quest;
@@ -41,26 +43,34 @@ function QuestsPage() {
 
   return (
     <>
-    <div className="ml-5 mt-10 w-70 ">
-    <div className="flex justify-even gap-5 flex-wrap rounded-3xl">
-        {quests.map((quest) => (
-          <div className=" bg-green-950 rounded-3xl flex flex-col p-5 shadow-2xl" key={quest.id}>
-            <div>
-            <h2 className="self-start">Difficulty Level: {quest.level}</h2>
+      <div className="ml-5 mt-10 w-70 ">
+        <div className="flex justify-even gap-5 flex-wrap rounded-3xl">
+          {quests.map((quest) => (
+            <div
+              className=" bg-primary rounded-3xl flex flex-col p-5 shadow-2xl"
+              key={quest.id}
+            >
+              <div>
+                <h2 className="self-start">Difficulty Level: {quest.level}</h2>
+              </div>
+              <div className="w-60">
+                <p className="bg-primary max-w-sm mt-4 mb-4 ">
+                  {capitalizeWords(quest.text)}
+                </p>
+              </div>
+              <div className="flex flex-col justify-between text-center mt-auto">
+                <button
+                  className="bg-button p-1 shadow-xl w-auto"
+                  onClick={() => handleChoice(quest.id)}
+                >
+                  ACCEPT THIS QUEST
+                </button>
+              </div>
             </div>
-            <div className="w-60">
-            <p className="bg-green-950 max-w-sm mt-4 mb-4 ">{capitalizeWords(quest.text)}</p>
-            </div>
-            <div className="flex flex-col justify-between text-center mt-auto">
-            <button className="bg-amber-500 p-1 shadow-xl w-auto" onClick={() => handleChoice(quest.id)}>
-              ACCEPT THIS QUEST
-            </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-      </div>
-      </>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import NavBar from "./components/NavBar.jsx";
 import "./input.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Profile from "./components/Profile.jsx";
 import Home from "./components/Home.jsx";
 import Layout from "./components/Layout.jsx";
-import Challenges from "./components/Challenges.jsx";
 import Facilities from "./components/Facilities.jsx";
 import Donation from "./components/Donation.jsx";
 import About from "./components/About.jsx";
@@ -17,25 +15,23 @@ import AuthProvider from "./components/AuthProvider.jsx";
 import Register from "./components/FakeRegister.jsx";
 import AddUserToDatabase from "./AddUserToDatabase.jsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/challenges" element={<Challenges />} />
-          <Route path="/facilities" element={<Facilities />} />
-          <Route path="/donate" element={<Donation />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/quests" element={<QuestsPage />} />
-          <Route path="/fakesignup" element={<Register />} />
-          <Route path="/test" element={<AddUserToDatabase />}/>
-        </Routes>
-      </Layout>
-    </Router>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/facilities" element={<Facilities />} />
+            <Route path="/donate" element={<Donation />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/quests" element={<QuestsPage />} />
+            <Route path="/fakesignup" element={<Register />} />
+            <Route path="/test" element={<AddUserToDatabase />} />
+          </Routes>
+        </Layout>
+      </Router>
     </AuthProvider>
   </React.StrictMode>
 );
