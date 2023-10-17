73ac78ae2176c34b1e7fcd6cda25071b3e70c673
Aleytys Isbell <isbellaleytys@gmail.com>
2023-10-17 09:34:05 -0400

    sign up and sign in styled

--- src/components/FakeSignIn.jsx
+++ src/components/FakeSignIn.jsx
@@ -1,70 +1,81 @@
-import { useRef, useState } from "react";
-import { useAuth } from "../components/AuthProvider"
+import { useRef, useState, useEffect } from "react";
+import { useAuth } from "../components/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
-
+import "./signin.css";
 
 const Login = () => {
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
-    const [errorMsg, setErrorMsg] = useState("");
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
-    const { login } = useAuth();
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        setErrorMsg("");
-        setLoading(true);
-        if (!passwordRef.current?.value || !emailRef.current?.value) {
-          setErrorMsg("Please fill in the fields");
-          return;
-        }
+  const loginSectionRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { login } = useAuth();
 
-        const {
-          data: { user, session},
-          error
-        } = await login(emailRef.current.value, passwordRef.current.value);
-        if (error) setErrorMsg(error.message);
-        if(user && session) navigate("/");
-      } catch (error) {
-        setErrorMsg("Email or Password Incorrect")
-      }
-      setLoading(false)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setErrorMsg("");
+      setLoading(true);
+      if (!passwordRef.current?.value || !emailRef.current?.value) {
+        setErrorMsg("Please fill in the fields");
+        return;
       }
 
+      const {
+        data: { user, session },
+        error,
+      } = await login(emailRef.current.value, passwordRef.current.value);
+      if (error) setErrorMsg(error.message);
+      if (user && session) navigate("/");
+    } catch (error) {
+      setErrorMsg("Email or Password Incorrect");
+    }
+    setLoading(false);
+  };
 
-      return (
-        <>
-          <div>
-            <h2 className="text-center mb-4">Login</h2>
-            <form onSubmit={handleSubmit}>
-              <div id="email">
-                <label>Email</label>
-                <input type="email" ref={emailRef} required />
+  useEffect(() => {
+    if (loginSectionRef.current) {
+      loginSectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
+  return (
+    <>
+      <div
+        ref={loginSectionRef}
+        className="bg-primary w-3/5 rounded-3xl flex justify-center mt-24 mb-24"
+      >
+        <div>
+          <h2 className="text-center mb-20 text-6xl mt-16">Sign In</h2>
+          <form onSubmit={handleSubmit}>
+            <div id="email" className="flex">
+              <label>Email</label>
+              <input type="email" ref={emailRef} required />
+            </div>
+            <div id="password" className="flex">
+              <label>Password</label>
+              <input type="password" ref={passwordRef} required />
+            </div>
+            {errorMsg && (
+              <div className="alert alert-danger" role="alert">
+                {errorMsg}
               </div>
-              <div id="password">
-                <label>Password</label>
-                <input type="password" ref={passwordRef} required />
-              </div>
-              {errorMsg && (
-                <div className="alert alert-danger" role="alert">
-                  {errorMsg}
-                </div>
-              )}
-              <div className="text-center mt-2">
-                <button
-                  disabled={loading}
-                  type="submit"
-                  className="w-50 btn btn-primary"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </>
-      );
-    };
+            )}
+            <div className="text-center mt-20">
+              <button
+                disabled={loading}
+                type="submit"
+                className="w-full shadow-xl bg-button"
+              >
+                SIGN IN
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
