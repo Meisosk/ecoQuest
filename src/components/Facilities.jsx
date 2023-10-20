dae53496c67c842fe2301c1c0b4cb4c35f824a1e
Meir Soskil <meirsoskil@gmail.com>
2023-10-20 12:03:29 -0400

    facilities mobile responisve

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -21,17 +21,26 @@ function Facilities() {
   };
 
   return (
-    <div className=" w-full h-full">
+    <div className=" w-full h-screen">
       <div>
         <SearchBar data={facilities} onFilterResults={handleFilterResults} />
       </div>
       <div className="flex flex-col w-full h-full ">
         {filteredFacilities.map((facility, index) => (
-          <div key={index} className="flex h-1/6 w-3/4 mb-1.7 ml-1.7">
-            <div className="bg-primary h-full w-full rounded-3xl p-5">
-              <p>Name: {facility.FacilityName}</p>
-              <p>GHG QUANTITY (METRIC TONS CO2e): {facility.Emissions}</p>
-              <p>Location: {facility.STATE}</p>
+          <div key={index} className="flex w-3/4 mb-1.7 ml-1.7">
+            <div className="bg-primary flex flex-col w-full rounded-3xl p-5 mb-1.7">
+              <p className="mb-3">
+                Name:&nbsp;&nbsp;&nbsp;
+                <span className="text-xl">{facility.FacilityName}</span>
+              </p>
+              <p className="mb-3">
+                GHG QUANTITY (METRIC TONS CO2e):&nbsp;&nbsp;&nbsp;
+                <span className=" text-2xl">{facility.Emissions}</span>
+              </p>
+              <p className="mb-3">
+                Location:&nbsp;&nbsp;
+                <span className=" text-lg">{facility.STATE}</span>{" "}
+              </p>
             </div>
           </div>
         ))}
