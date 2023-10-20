c97f80c99e292e7dc41fa68187d43dcfcd935622
Aleytys Isbell <isbellaleytys@gmail.com>
2023-10-20 17:18:17 -0400

    profile route protected and goes straight to home page

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -1,14 +1,15 @@
 import { useAuth } from "./AuthProvider"
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import Profile from "./Profile";
 
 const AuthRoute = () => {
   const { user } = useAuth();
   const location = useLocation();
 
   return user ? (
-    <Outlet /> //outlet is a placeholder component. where the component matches the current route to be rendered
+    <Profile /> //outlet is a placeholder component. where the component matches the current route to be rendered
   ) : (
-    <Navigate to={"/"} replace state={{ path: location.pathname }} />
+    <Navigate to={"/fakesignup"} replace state={{ path: location.pathname }} />
   );
 };
 
